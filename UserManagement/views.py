798be4843d5c3a76c85914617eef17c1c0717bf6
Sammy-Barasa <kesammybarasa@gmail.com>
2022-11-24 03:45:39 +0300

    deploy to heroku

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -5,3 +5,15 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def index(request):
     return Response({"Message": "UserAPIEndpoint"})
+
+# class RegisterView(generics.GenericAPIView):
+#     serializer_class = RegisterSerializer
+
+#     def post(self, request):
+#         user = request.data
+#         serializer = self.serializer_class(data=user)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         user_data = serializer.validated_data
+#         # use saved email to send verifying link
+#         validated_user = User.objects.get(email=user_data['email'])
--- dronprojectcore/settings.py
+++ dronprojectcore/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'dronprojectcore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
